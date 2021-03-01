8ec8b5d0f6dac26fc675253bf8af6d648c300370
wangze <zacharywong880@outlook.com>
2021-03-01 17:02:15 +0800

    王道机试

---
+++ 12-动态规划/12.1-递归求解.cpp
@@ -0,0 +1,57 @@
+//
+// Created by 王泽 on 2021/3/1.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+const int MAXN = 100;
+// 朴素递归，时间复杂度 O(2^n)
+int Fibonacci1(int n){
+    int ans;
+    if (n==0 || n==1){
+        ans = n;
+    } else{
+        ans = Fibonacci1(n-1) + Fibonacci1(n-2);
+    }
+    return ans;
+}
+// 自顶向下朴素递归+记忆化（记忆化搜索）, 时间复杂度 O(n)
+int memo[MAXN];
+int Fibonacci2(int n){
+    if (memo[n] != -1){
+        return memo[n];
+    }
+    int ans;
+    if (n==0 || n==1){
+        ans = n;
+    } else{
+        ans = Fibonacci2(n-1) + Fibonacci2(n-2);
+    }
+    memo[n] = ans;
+    return ans;
+}
+//自底向上基于递推求解，与Fibonacci2本质上一致
+int fib[MAXN];
+int Fibonacci3(int n){
+    for (int i = 0; i <= n; ++i) {
+        int ans;
+        if (i == 0 || i == 1){
+            ans = n;
+        }else{
+            ans = fib[i-1] + fib[i-2];
+        }
+        fib[i] = ans;
+    }
+    return fib[n];
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << Fibonacci1(n) << endl;
+    fill(memo, memo + MAXN, -1);
+    cout << Fibonacci2(n) << endl;
+    return 0;
+}
